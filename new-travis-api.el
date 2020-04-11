d1e8e31a9fbb1e1ff79491ca6ba58902d690d521
AuPath <marco.au.grassi98@protonmail.com>
2020-04-11 20:25:29 +0200

    Implemented working get builds for repo

--- new-travis-api.el
+++ new-travis-api.el
@@ -34,9 +34,13 @@
   "Return url to repositories owned by USER-LOGIN."
   (format "%s/owner/%s/repos" travis-api-url user-login))
 
-(defun travis-url-builds-for-repo (repo-slug)
-  "Return url for getting builds for REPO-SLUG."
-  (format "%s/repo/%s/builds" travis-api-url (url-hexify-string repo-slug)))
+(defun travis-url-builds-for-repo (id)
+  "Return url for getting builds for repo with ID."
+  (format "%s/repo/%s/builds" travis-api-url id))
+
+(defun travis-url-active-builds (user-login)
+  "Return url for getting active builds for user USER-LOGIN."
+  (format "%s/owner/%s/active" travis-api-url user-login))
 
 ;; VARIABLES
 (defvar travis-token nil)
@@ -71,6 +75,36 @@
   (interactive)
   (message "Travis token: %s" travis-token))
 
+(defun travis-show-builds-for-repo ()
+  "Show builds for specified repo."
+  (interactive)
+  (travis-request-user-repos)
+  (request
+    (travis-url-builds-for-repo (travis-get-repo-id-from-name
+				 (ido-completing-read
+				  "Available repositories: "
+				  (travis-repos-name))))
+    :type "GET"
+    :headers travis-headers
+    :parser (lambda ()
+           (let ((json-array-type 'list))
+             (json-read)))
+    :success (cl-function
+	      (lambda (&key data &allow-other-keys)
+		(travis-show-buffer-with-data "*BUILDS*"
+					      (mapconcat 'travis-build-to-string
+							 (assoc-default 'builds data)
+							 "\n\n"))))))
+;; HELPER FUNCTIONS
+
+(defun travis-repos-name ()
+  "Return a list containg the name of the repos owned by the user."
+  (mapcar 'first travis-user-repos-verbose))
+
+(defun travis-get-repo-id-from-name (repo-name)
+  "Return repo id for repo REPO-NAME."
+  (assoc-default repo-name travis-user-repos-verbose))
+
 (defun travis-request-user-repos ()
   "Request user owned repos for user stored in owner-repos."
   (request
@@ -81,32 +115,28 @@
     :sync t
     :success (cl-function
 	      (lambda (&key data &allow-other-keys)
-		(setq travis-user-repos-verbose (mapcar (lambda (x) (cons (assoc-default 'name x) (assoc-default 'slug x))) (assoc-default 'repositories data)))))))
+		(setq travis-user-repos-verbose (mapcar (lambda (x) (cons
+								     (assoc-default 'name x)
+								     (assoc-default 'id x)))
+							(assoc-default 'repositories data)))))))
 
-(defun travis-request-builds-for-repo ()
-  "Request builds for specified repo."
-  (request
-    (travis-url-builds-for-repo (travis-get-repo-slug-from-name
-				 (ido-completing-read "Available repositories: " (travis-repos-name))))
-    :type "GET"
-    :header travis-headers
-    :parser 'json-read
-    :success (cl-function
-	      (lambda (&key data &allow-other-keys)
-		(message "prova")))))
-
-(travis-request-builds-for-repo)
-(travis-request-user-repos)
-
-(defun travis-repos-name ()
-  "Return a list containg the name of the repos owned by the user."
-  (mapcar 'first travis-user-repos-verbose))
-
-(defun travis-get-repo-slug-from-name (repo-name)
-  "Return repo slug for repo REPO-NAME."
-  (assoc-default repo-name travis-user-repos-verbose))
+(defun travis-build-to-string (build)
+  "Return string to insert into display buffer for BUILD."
+  (concat (format "Build for repository: %s\n" (assoc-default 'name (assoc-default 'repository build)))
+	  (format "Build for branch: %s\n" (assoc-default 'name (assoc-default 'branch build)))
+	  (format "Build for commit: %s\n" (assoc-default 'id (assoc-default 'commit build)))
+	  (format "Build ID: %s\n" (assoc-default 'id build))
+	  (format "Build number: %s\n" (assoc-default 'number build))
+	  (format "Build state: %s\n" (assoc-default 'state build))
+	  (format "Build previous state: %s\n" (assoc-default 'previous_state build))
+	  (format "Build started at: %s\n" (assoc-default 'started_at build))
+	  (format "Build finished at: %s\n" (assoc-default 'finished_at build))
+	  (format "Build took: %s\n" (format-seconds
+				      "%H %M %S"
+				      (assoc-default 'duration build)))))
 
 (defun travis-show-buffer-with-data (buffer-name data)
+  "Open a buffer named BUFFER-NAME and insert DATA."
   (with-current-buffer (get-buffer-create buffer-name)
     (erase-buffer)
     (insert data)
